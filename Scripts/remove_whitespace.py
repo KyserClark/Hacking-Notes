69fd875bf39383b1913208ebd2e44dcdc37e7880
Kyser Clark <92742375+KyserClark@users.noreply.github.com>
2023-06-02 19:21:32 -0800

    Create remove_whitespace.py

---
+++ Scripts/remove_whitespace.py
@@ -0,0 +1,29 @@
+"""
+Written by Kyser Clark
+
+KyserClark.com
+@KyserClark
+github.com/KyserClark
+linkedin.com/in/KyserClark/
+
+This python script takes any .txt file and removes all whitespace from it.
+"""
+
+import sys
+
+def remove_whitespace(filename):
+    with open(filename, 'r') as file:
+        content = file.read()
+        content = content.replace('\n', '').replace('\r', '').replace(' ', '')
+    
+    with open(filename, 'w') as file:
+        file.write(content)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please provide the filename as an argument.")
+    else:
+        filename = sys.argv[1]
+        remove_whitespace(filename)
+        print(f"Whitespace removed from {filename}.")
